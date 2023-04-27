61adfad5b7f52764ca2f2534cf09e2ec09543924
neusfer <125355473+neusfer@users.noreply.github.com>
2023-04-27 09:22:28 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+radio.setGroup(1)
+music.setVolume(35)
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Strength) < 600) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    } else {
+        radio.sendNumber(0)
+        music.stopAllSounds()
+        basic.clearScreen()
+    }
+})
