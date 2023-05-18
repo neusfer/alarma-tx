743c0328f4bc978bab332bf65e30c6f76fe1c68f
neusfer <125355473+neusfer@users.noreply.github.com>
2023-05-18 09:14:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) < 600) {
         radio.sendNumber(1)
         basic.showLeds(`
+            # # # # #
             # . . . #
-            # # . . #
             # . # . #
-            # . . # #
             # . . . #
+            # # # # #
             `)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
         basic.pause(200)
